03172aedf11da58a01fa826f7a3253ecf88d240d
HS\himanshu.rai <himanshu.rai@heliossolutions.co>
2023-02-24 01:59:53 +0530

    Changes for Admin Layout

--- Bioworld/Bioworld/Program.cs
+++ Bioworld/Bioworld/Program.cs
@@ -1,8 +1,11 @@
 using Bioworld.Service;
 using Business.Repository;
 using DataAccess.Data;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +14,14 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders().AddDefaultUI()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+        options.SignIn.RequireConfirmedAccount = false;
+        options.SignIn.RequireConfirmedEmail = false;
+    }).AddDefaultTokenProviders().AddDefaultUI()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddScoped<IProductPriceRepository, ProductPriceRepository>();
---
+++ Bioworld/DataAccess/Models/ApplicationRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+
+        public string UpdatedByUserId { get; set; }
+
+        public DateTime UpdatedDateUTC { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Maximum of 500 characters allowed.")]
+        public string Description { get; set; }
+
+        public ICollection<ApplicationUserRole> UserRoles { get; set; }
+    }
+}
