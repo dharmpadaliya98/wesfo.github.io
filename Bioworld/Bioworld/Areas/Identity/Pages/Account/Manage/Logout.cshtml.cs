aec61fd611b75c741cf398ac2ac06d9b8eb6fa82
HS\himanshu.rai <himanshu.rai@heliossolutions.co>
2023-03-06 22:34:55 +0530

    minor fixes

--- Bioworld/Bioworld/Areas/Identity/Pages/Account/Manage/Logout.cshtml.cs
+++ Bioworld/Bioworld/Areas/Identity/Pages/Account/Manage/Logout.cshtml.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DataAccess.Models;
+﻿using DataAccess.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace TangyWeb_Server.Areas.Identity.Pages.Account.Manage
+namespace Bioworld.Areas.Identity.Pages.Account.Manage
 {
     [AllowAnonymous]
     [IgnoreAntiforgeryToken]
